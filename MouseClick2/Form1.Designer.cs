54a3656d3d8aa2628e93878603f59f313bf39e52
想吃大地豆 <128659148+xiangchidadidou@users.noreply.github.com>
2024-03-18 15:29:26 +0800

    直接提交
    主程序打包到了debug里，初次打开需要安装.net8环境

---
+++ MouseClick2/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace MouseClick2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            button1 = new Button();
+            label12 = new Label();
+            label13 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox1 = new TextBox();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 20);
+            label1.TabIndex = 0;
+            label1.Text = "手动";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(96, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Q 睡觉";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(157, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 20);
+            label3.TabIndex = 1;
+            label3.Text = "W 吃饭";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(221, 71);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 20);
+            label4.TabIndex = 1;
+            label4.Text = "E 上课卓";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 71);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 20);
+            label5.TabIndex = 2;
+            label5.Text = "人多：";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(96, 108);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 20);
+            label6.TabIndex = 1;
+            label6.Text = "A 教师人少";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(186, 108);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 20);
+            label7.TabIndex = 1;
+            label7.Text = "S 无人";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(244, 108);
+            label8.Name = "label8";
+            label8.Size = new Size(69, 20);
+            label8.TabIndex = 1;
+            label8.Text = "D 下一页";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(37, 108);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 20);
+            label9.TabIndex = 2;
+            label9.Text = "人少：";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(416, 144);
+            button1.Name = "button1";
+            button1.Size = new Size(225, 30);
+            button1.TabIndex = 4;
+            button1.Text = "L 自动标注无人";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(416, 32);
+            label12.Name = "label12";
+            label12.Size = new Size(39, 20);
+            label12.TabIndex = 0;
+            label12.Text = "自动";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(293, 70);
+            label13.Name = "label13";
+            label13.Size = new Size(68, 20);
+            label13.TabIndex = 1;
+            label13.Text = "R 上课椅";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(416, 70);
+            label10.Name = "label10";
+            label10.Size = new Size(288, 20);
+            label10.TabIndex = 0;
+            label10.Text = "屏幕分辨率须在1920*1080 网站大小75%";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(416, 108);
+            label11.Name = "label11";
+            label11.Size = new Size(250, 20);
+            label11.TabIndex = 0;
+            label11.Text = "页数需要隔开前后10页 按钮间隔3秒";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(416, 246);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 27);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(416, 219);
+            label14.Name = "label14";
+            label14.Size = new Size(99, 20);
+            label14.TabIndex = 6;
+            label14.Text = "当前间隔时间";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(521, 219);
+            label15.Name = "label15";
+            label15.Size = new Size(18, 20);
+            label15.TabIndex = 6;
+            label15.Text = "0";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(521, 249);
+            label16.Name = "label16";
+            label16.Size = new Size(39, 20);
+            label16.TabIndex = 6;
+            label16.Text = "毫秒";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(566, 246);
+            button2.Name = "button2";
+            button2.Size = new Size(77, 27);
+            button2.TabIndex = 7;
+            button2.Text = "修改";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label15);
+            Controls.Add(label16);
+            Controls.Add(label14);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label9);
+            Controls.Add(label5);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label13);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label12);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "自动化标注";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Button button1;
+        private Label label12;
+        private Label label13;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox1;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Button button2;
+    }
+}
---
+++ MouseClick2/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClick2
+{
+    public partial class Form1 : Form
+    {
+        //设置鼠标进制数
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        //声明线程及其运行、启动
+        private Thread KeyPressListening = null;
+        private bool isRunning = true;
+        bool isStar = false;
+        //声明自动化线程的运行、启动
+        private Thread AutoMouseThread = null;
+        private bool isAutoSart = false;
+        private bool isAutoRunning = false;
+        //键盘业务
+        private bool isQ = false;
+        private bool isW = false;
+        private bool isE = false;
+        private bool isR = false;
+        private bool isA = false;
+        private bool isS = false;
+        private bool isD = false;
+        private bool isL = false;
+        private bool isP = false;
+        //按键间隔时间
+        int keySleepTimer = 500;
+        int autoSleepTimer = 100;
+        //声明数组
+        int[,] allPosition = { };
+        int[,] chifan = { { 1399, 346 }, { 1424, 421 }, { 1388, 496 }, { 1467, 619 }, { 1399, 691 }, { 1515, 770 }, { 1565, 847 }, { 1591, 924 }, { 1394, 988 } };
+        int[,] shuijiao = { { 1398, 343 }, { 1425, 416 }, { 1763, 496 }, { 1426, 622 }, { 1494, 692 }, { 1776, 771 }, { 1563, 844 }, { 1586, 924 }, { 1395, 986 } };
+        int[,] shangke1 = { { 1401, 345 }, { 1424, 416 }, { 1561, 494 }, { 1632, 494 }, { 1469, 615 }, { 1401, 693 }, { 1518, 770 }, { 1566, 847 }, { 1589, 924 }, { 1395, 987 } };
+        int[,] shangke2 = { { 1399, 341 }, { 1424, 418 }, { 1563, 493 }, { 1632, 494 }, { 1469, 613 }, { 1489, 694 }, { 1387, 769 }, { 1565, 846 }, { 1590, 925 }, { 1402, 985 } };
+        int[,] jiaoshirenshao = { { 1488, 342 }, { 1422, 418 }, { 1633, 490 }, { 1380, 617 }, { 1490, 692 }, { 1775, 769 }, { 1564, 847 }, { 1587, 924 }, { 1395, 991 } };
+        int[,] wuren = { { 1557, 343 }, { 1401, 990 }, { 783, 815 } };
+        int[,] xiayiye = { { 1397, 988 }, { 783, 815 } };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //指示WinApi动态链接库导入
+        [DllImport("user32.dll")]
+        private static extern short GetKeyState(int key);
+
+        //声明WinApi属性的mouse方法
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        //声明WinApi属性的bool与c#bool属性的转化，并且声明鼠标定位方法
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int X, int Y);
+
+
+        public static void MoveAndClick(int x, int y)
+        {
+            // 移动鼠标到指定位置  
+            SetCursorPos(x, y);
+
+            // 模拟鼠标左键按下  
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            // 模拟鼠标左键抬起  
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+
+        //创建按键获取函数
+        private bool IsKeyPressed(Keys key)
+        {
+            return (GetKeyState((int)key) & 0x8000) != 0;
+        }
+
+
+        //线程函数
+        private void KeyListenedThread()
+        {
+            try
+            {
+                while (isRunning)
+                {
+                    //
+                    if (IsKeyPressed(Keys.Q))
+                    {
+                        if (!isQ)
+                        {
+                            for (int i = 0; i < shuijiao.GetLength(0); i++)
+                            {
+                                int x = shuijiao[i, 0]; // 获取 X 坐标
+                                int y = shuijiao[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isQ = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.W))
+                    {
+                        if (!isW)
+                        {
+                            for (int i = 0; i < chifan.GetLength(0); i++)
+                            {
+                                int x = chifan[i, 0]; // 获取 X 坐标
+                                int y = chifan[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isW = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.E))
+                    {
+                        if (!isE)
+                        {
+                            for (int i = 0; i < shangke1.GetLength(0); i++)
+                            {
+                                int x = shangke1[i, 0]; // 获取 X 坐标
+                                int y = shangke1[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isE = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.R))
+                    {
+                        if (!isR)
+                        {
+                            for (int i = 0; i < shangke2.GetLength(0); i++)
+                            {
+                                int x = shangke2[i, 0]; // 获取 X 坐标
+                                int y = shangke2[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isR = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.A))
+                    {
+                        if (!isA)
+                        {
+                            for (int i = 0; i < jiaoshirenshao.GetLength(0); i++)
+                            {
+                                int x = jiaoshirenshao[i, 0]; // 获取 X 坐标
+                                int y = jiaoshirenshao[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isA = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.S))
+                    {
+                        if (!isS)
+                        {
+                            for (int i = 0; i < wuren.GetLength(0); i++)
+                            {
+                                int x = wuren[i, 0]; // 获取 X 坐标
+                                int y = wuren[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isS = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.D))
+                    {
+                        if (!isD)
+                        {
+                            for (int i = 0; i < xiayiye.GetLength(0); i++)
+                            {
+                                int x = xiayiye[i, 0]; // 获取 X 坐标
+                                int y = xiayiye[i, 1]; // 获取 Y 坐标
+                                MoveAndClick(x, y);
+                            }
+                        }
+                        else
+                        {
+                            isD = false;
+                        }
+                        Thread.Sleep(keySleepTimer);
+                    }
+                    //
+                    if (IsKeyPressed(Keys.L))
+                    {
+                        if (!isL)
+                        {
+                            if (!isAutoRunning)
+                            {
+                                button1.ForeColor = Color.Green;
+                                Thread AutoMouseThread = new Thread(AutoThread);
+                                AutoMouseThread.Start();
+                                isAutoRunning = true;
+                            }
+                            else
+                            {
+                                button1.ForeColor = Color.Black;
+                                isL = false;
+                                isAutoRunning = false;
+                                if (AutoMouseThread != null && AutoMouseThread.IsAlive)
+                                {
+                                    AutoMouseThread.Join();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            button1.ForeColor = Color.Black;
+                            isL = false;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                    Application.DoEvents();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+
+            }
+
+        }
+
+
+        //自动化函数
+        private void AutoThread()
+        {
+            try
+            {
+                while (isAutoRunning)
+                {
+                    for (int i = 0; i < wuren.GetLength(0); i++)
+                    {
+                        int x = wuren[i, 0]; // 获取 X 坐标
+                        int y = wuren[i, 1]; // 获取 Y 坐标
+                        MoveAndClick(x, y);
+                    }
+                    Thread.Sleep(autoSleepTimer);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+
+            }
+        }
+
+        //窗体加载函数
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyPressListening = new Thread(KeyListenedThread);
+            KeyPressListening.Start();
+
+            this.FormClosing += Form1_FormClosing;
+        }
+
+
+        //窗体关闭函数
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isAutoRunning = false;
+            if (AutoMouseThread != null && AutoMouseThread.IsAlive)
+            {
+                AutoMouseThread.Join();
+            }
+
+            isRunning = false;
+            if (KeyPressListening != null && KeyPressListening.IsAlive)
+            {
+                KeyPressListening.Join();
+            }
+
+        }
+
+
+        //P按钮函数
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isStar = !isStar;
+            if (isStar)
+            {
+                button1.ForeColor = Color.Red;
+            }
+            else
+            {
+                button1.ForeColor = Color.Green;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label15.Text = autoSleepTimer.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+
+            // 检查输入是否是有效数字，如果不是则清除文本框内容
+            if (!string.IsNullOrEmpty(text) && !int.TryParse(text, out _))
+            {
+                textBox.Text = ""; // 清除文本框内容
+            }
+            else if (!string.IsNullOrEmpty(text))
+            {
+                int number;
+                if (int.TryParse(text, out number))
+                {
+                    autoSleepTimer = number;
+                }
+            }
+        }
+    }
+}
